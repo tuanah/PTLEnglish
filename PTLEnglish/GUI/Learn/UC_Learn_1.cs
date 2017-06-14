ba23196c2f39854236ce4190242228ea32fc337d
Nguyen Hoang Anh Tuan <tuanlaw25@gmail.com>
2017-06-15 00:48:09 +0700

    adsda

---
+++ PTLEnglish/GUI/Learn/UC_Learn_1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTLEnglish.GUI.Learn
+{
+	public partial class UC_Learn_1 : UserControl
+	{
+		public UC_Learn_1()
+		{
+			InitializeComponent();
+		}
+	}
+}
